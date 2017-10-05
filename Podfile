8079de5a3190318e3f46f64d22c4636aef205632
LemonaInc <jaxonstevens@lemonainc.com>
2017-10-04 19:27:02 -0700

    Added JSSAlerts

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'CabriolaApp' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-
+pod 'JSSAlertView'
 pod 'FacebookCore', '~> 0.2'
 pod 'FacebookLogin', '~> 0.2'
 pod 'FBNotifications'
--- Pods/Target Support Files/Pods-CabriolaApp/Pods-CabriolaApp-frameworks.sh
+++ Pods/Target Support Files/Pods-CabriolaApp/Pods-CabriolaApp-frameworks.sh
@@ -108,6 +108,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FBSDKLoginKit/FBSDKLoginKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FacebookCore/FacebookCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FacebookLogin/FacebookLogin.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/JSSAlertView/JSSAlertView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
@@ -116,6 +117,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FBSDKLoginKit/FBSDKLoginKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FacebookCore/FacebookCore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FacebookLogin/FacebookLogin.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/JSSAlertView/JSSAlertView.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
